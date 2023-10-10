14a47dc8aa4facb2745869a970f78dd509ad19d6
EthanBaldwin22 <70599620+EthanBaldwin22@users.noreply.github.com>
2023-10-10 11:58:01 -0400

    Update get_organelle_batch.sh

--- scripts/plastome_assembly/get_organelle_batch.sh
+++ scripts/plastome_assembly/get_organelle_batch.sh
@@ -9,18 +9,17 @@
 #SBATCH --output=/scratch/eab77806/logs/get_organelle.%j.out
 #SBATCH --error=/scratch/eab77806/logs/get_organelle.%j.err
 
-
-INDIR="/scratch/eab77806/trim_reads/trimmed_reads"
-if [ ! -d $INDIR ]
-then
-    mkdir -p $INDIR
-fi
-cd $INDIR
-
+# load software
 ml GetOrganelle/1.7.5.2-foss-2020b
 
+# input directory with reads from all samples
+INDIR="/scratch/eab77806/trim_reads/trimmed_reads"
+cd $INDIR
+
+# create array of sample names; in this case all of my sample names are the first four characters of the file names
 mapfile -t SAMPLE < <(for f in *gz; do echo $f | cut -b 1-4; done | uniq)
 
+# create output directory if it does not exist, and change directory into it
 OUTDIR="/scratch/eab77806/sarracenia_plastome"
 if [ ! -d $OUTDIR ]
 then
@@ -28,6 +27,7 @@ then
 fi
 cd $OUTDIR
 
+# loop through the array SAMPLE, running get_organelle for each one
 for i in "${SAMPLE[@]}"
 do get_organelle_from_reads.py -1 $INDIR/${i}_P_R1.fastq.gz -2 $INDIR/${i}_P_R2.fastq.gz  -o ${i}.plastome_output -k 21,45,65,85,97,105,121 -F embplant_pt
 done
